ba568953674f0af9b7c47f4ca712ec2e4feb164b
apple <apple@appledeMacBook-Pro.local>
2020-05-10 23:45:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <router-link :to="'/'" class="nav-link">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'HelloWorld2' }" class="nav-link">chat</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="'hello2'" class="nav-link">Hello2</router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello container">
+    <div class="media" v-for="item in messages">
+      <img v-if="item.username != username" src="https://fakeimg.pl/60/" class="align-self-start mr-3" alt="...">
+      <div class="media-body">
+        <h5 class="mt-0">{{ item.username }}</h5>
+        <p>{{ item.message }}</p>
+      </div>
+      <img v-if="item.username == username" src="https://fakeimg.pl/60/" class="align-self-start mr-3" alt="...">
+    </div>
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" v-model="message" placeholder="輸入訊息" aria-label="輸入訊息" aria-describedby="button-addon2">
+      <div class="input-group-append">
+        <button class="btn btn-outline-primary" @click="submitMessage" type="button" id="button-addon2">送出</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld2',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      username: 'Amy',
+      message: '',
+      messages: [
+        {
+          username: 'Amy',
+          message: 'Hello 你好'
+        },
+        {
+          username: 'John',
+          message: '你也好'
+        }
+      ]
+    }
+  },
+  methods: {
+    submitMessage () {
+      let vm = this
+      vm.messages.push({
+        username: vm.username,
+        message: vm.message
+      })
+      vm.message=''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
